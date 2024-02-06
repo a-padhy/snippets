7663237b1186912a4827b197d975c2af92c6afd1
Ashutosh Padhy <ashutoshpadhy001@gmail.com>
2024-02-06 16:17:43 +0530

    add caching

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,8 @@
-'use server';
+"use server";
 
-import { redirect } from 'next/navigation';
-import { db } from '@/db';
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { db } from "@/db";
 
 export async function editSnippet(id: number, code: string) {
   await db.snippet.update({
@@ -9,6 +10,7 @@ export async function editSnippet(id: number, code: string) {
     data: { code },
   });
 
+  revalidatePath(`/snippets/${id}`);
   redirect(`/snippets/${id}`);
 }
 
@@ -17,5 +19,55 @@ export async function deleteSnippet(id: number) {
     where: { id },
   });
 
-  redirect('/');
+  // on demand caching
+  revalidatePath("/");
+  redirect("/");
+}
+
+//1st paramater always formState
+export async function createSnippet(
+  formState: { message: string },
+  formData: FormData
+) {
+  try {
+    // Check the user's inputs and make sure they're valid
+    const title = formData.get("title");
+    const code = formData.get("code");
+
+    if (typeof title !== "string" || title.length < 3) {
+      return {
+        message: "Title must be longer",
+      };
+    }
+    if (typeof code !== "string" || code.length < 10) {
+      return {
+        message: "Code must be longer",
+      };
+    }
+
+    // Create a new record in the database
+    await db.snippet.create({
+      data: {
+        title,
+        code,
+      },
+    });
+  } catch (err: unknown) {
+    // always pass errors as plain messagesm useFormState will take care!
+    if (err instanceof Error) {
+      return {
+        message: err.message,
+      };
+    } else {
+      return {
+        message: "Something went wrong...",
+      };
+    }
+  }
+
+  // on demand caching
+  revalidatePath("/");
+
+  // Redirect the user back to the root route
+  redirect("/"); // Never use this in try-catch. Next will throw the special Error!
 }
